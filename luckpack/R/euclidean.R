839608f4ad06a0d6f6d2eb1dc7bfc6b93b7a3225
codercola-mw <meconho@gmail.com>
2019-09-12 20:54:01 +0200

    "Roxy "

--- luckpack/R/euclidean.R
+++ luckpack/R/euclidean.R
@@ -1,4 +1,11 @@
-#1.1.1 euclidean()
+#' Create E Alg
+#' 
+#' @param x A numberical value
+#' @param y A numberical value
+#' @return The greatest common divisior
+#' @examples
+#' euclidean(1, 1)
+#' euclidean(10, 1)
 euclidean <- function(x,y){
   r <- x%%y;
   return(ifelse(r, euclidean(y, r), y))
