f323862485a31111d963045b95b4463469e3baaa
Mahbuba Haque <mahbuba.haque@ki.se>
2019-09-13 13:44:14 +0200

    "include wiki_graph"

---
+++ luckpack/R/data.R
@@ -0,0 +1,13 @@
+#' Graph containing the edges and the weight of the edges.
+#'
+#' A data frame containing three variables; the edges of a graph and 
+#' the weight of the edges.
+#'
+#' @format A data frame with 18 rows and 3 variables:
+#' \describe{
+#'   \item{v1}{edges}
+#'   \item{v2}{edges}
+#'   \item{w}{weight of the edges}
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
+"wiki_graph"
