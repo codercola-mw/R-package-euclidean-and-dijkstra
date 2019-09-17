eefcb231c2f3e87d67a86c720afd3a34d068c482
Mahbuba Haque <mahbuba.haque@ki.se>
2019-09-17 13:01:45 +0200

    "edited Euclidean"

--- luckpack/R/euclidean.R
+++ luckpack/R/euclidean.R
@@ -1,15 +1,13 @@
-#' Create E Alg
+#' The Euclidean Algorithm
 #' 
-#' @param x A numberical value
-#' @param y A numberical value
-#' @return The greatest common divisior
+#' @param x A numerical value
+#' @param y A numerical value
+#' @return The greatest common divisior of two numbers
 #' @examples
-#' euclidean(1, 1)
-#' euclidean(10, 1)
+#' euclidean(123612, 13892347912)
+#' euclidean(100, 1000)
 #' @export
 euclidean <- function(x,y){
   r <- x%%y;
   return(ifelse(r, euclidean(y, r), y))
 }
-
-
