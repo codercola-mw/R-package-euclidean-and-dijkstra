2e232f426a778247eb274d46ca28e889a5833806
Mahbuba Haque <mahbuba.haque@ki.se>
2019-09-18 17:22:36 +0200

    "updated rd"

--- luckpack/R/dijkstra.R
+++ luckpack/R/dijkstra.R
@@ -1,11 +1,15 @@
-#' Create Dijkstras Alogrithm
+#' Dijkstras Alogrithm
 #' 
-#' @param graph A numberical value
-#' @param init_node A numberical value
-#' @return The shortest path
+#' @param graph A graph
+#' @param init_node A numerical value
+#' @return The shortest path from the initial node to every other node in the graph
 #' @examples
-#' dijkstra(graph = data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9)), 1)
-#' dijkstra(graph = data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9)), 3)
+#' dijkstra(graph = data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
+#' v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
+#' w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9)), 1)
+#' dijkstra(graph = data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
+#' v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
+#' w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9)), 3)
 #' 
 #' @export
 dijkstra<-function(graph,init_node){
